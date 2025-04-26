ba6344cbc2dd4badbe312fd23639d53dddae30f1
redanzo <66446416+redanzo@users.noreply.github.com>
2025-04-25 21:53:37 -0500

    redux installations - sidebar

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,40 +1,60 @@
 "use client";
 
 import React from "react";
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const toggleSideBar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  }
+
   return (
     <div className="flex justify-between item-center w-full mb-7">
       {/* LEFT SIDE */}
       <div className="flex justify-between items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSideBar}
         >
           <Menu className="w-4 h-4" />
         </button>
-      
-      <div className="relative">
-        <input
-          type="search"
-          placeholder="Start type to search groups & products"
-          className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
-        />
 
-        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
-          <Bell className="text-gray-500" size={20} />
+        <div className="relative">
+          <input
+            type="search"
+            placeholder="Start type to search groups & products"
+            className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+          />
+
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
+            <Bell className="text-gray-500" size={20} />
+          </div>
         </div>
-      </div>
       </div>
 
       {/* RIGHT SIDE */}
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button onClick={() => {}}>
-              <Sun className="cursor-pointer text-gray-500" size={24} />
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (<Sun className="cursor-pointer text-gray-500" size={24} />)
+              :
+              (<Moon className="cursor-pointer text-gray-500" size={24} />)}
+              
             </button>
           </div>
           <div className="relative">
@@ -50,7 +70,7 @@ const Navbar = () => {
           </div>
         </div>
         <Link href="/settings">
-            <Settings className="cursor-pointer text-gray-500" size={24} />
+          <Settings className="cursor-pointer text-gray-500" size={24} />
         </Link>
       </div>
     </div>
