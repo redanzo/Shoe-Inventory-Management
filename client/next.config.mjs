0a8a898179897ccaa1ab183873bbe9864efc1925
redanzo <66446416+redanzo@users.noreply.github.com>
2025-06-11 18:40:39 -0500

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "https://s3-rishibharthi-shoe-inventory-management.s3.us-east-2.amazonaws.com",
-                port: "",
-                pathname: "/**",
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-rishibharthi-shoe-inventory-management.s3.us-east-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
